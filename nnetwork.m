90a0053660df62c28b6cc193448896519a7d627f
VictorBarradas <vrbarradasp@gmail.com>
2015-07-27 14:39:14 -0700

    Started modifications to generalize construction of nnetwork and its subclasses objects to networks with n layers.

--- nnetwork.m
+++ nnetwork.m
@@ -2,14 +2,18 @@ classdef nnetwork < handle
     %Simple two-layer neural network
     %
     
-    properties              
+    properties
+        % These properties should be removed
         omegaInput = 5; %Width of input units RBF
         sigmaInputUnits; %SD of noise in network inputs
+        cInput %Centers of the input units
+        
+        nLayers % Number of layers in the network, including input layer
+        nUnits % Number of units in each layer, indicated by a vector
         nInput %Number of input units
         nOutput %Number of output units
-        cInput %Centers of the input units
-        W  %Weight matrix
-        type
+        W  % Cell array of weight matrices
+        type % type of neural network
     end
     
     methods (Abstract)
@@ -18,12 +22,16 @@ classdef nnetwork < handle
     end
     
     methods
-        function obj = nnetwork(nInput,nOutput)
-            obj.nInput = nInput;
-            obj.nOutput = nOutput;
-            obj.cInput = -180:360/nInput:180-360/nInput;
-            obj.W = zeros(nInput,nOutput);
-            obj.sigmaInputUnits = 0.0*ones(nInput,1);
+        function obj = nnetwork(nUnits)
+            obj.nUnits = nUnits;
+            obj.nLayers = length(obj.nUnits);
+            obj.nInput = obj.nUnits(1);
+            obj.nOutput = obj.nUnits(end);
+            obj.cInput = -180:360/obj.nInput:180-360/obj.nInput;
+            for i = 1:obj.nLayers - 1
+                obj.W{i} = zeros(obj.nUnits(i),obj.nUnits(i+1));
+            end
+            obj.sigmaInputUnits = 0.0*ones(obj.nInput,1);
         end
         
         function a = input_layer_activation(obj,direction,magnitude)
