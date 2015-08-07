c57c8dfd62f04f38ccffc51da4ab77d327b94531
VictorBarradas <vrbarradasp@gmail.com>
2015-08-07 15:34:41 -0700

    Added bias to layers of neural network.

--- nnetworkBP.m
+++ nnetworkBP.m
@@ -7,26 +7,33 @@ classdef nnetworkBP < nnetwork
         activationOutput
         layerOutput
         layerInput
+        isBias
     end
     
     methods
         function obj = nnetworkBP(nUnits)
             obj@nnetwork(nUnits);
             for i = 1:obj.nLayers - 1
-                obj.W{i} = normrnd(0,0.5,nUnits(i),nUnits(i+1));
+                obj.W{i} = normrnd(0,0.5,nUnits(i)+1,nUnits(i+1));
+                %obj.W{i} = normrnd(0,0.5,nUnits(i),nUnits(i+1));
             end
             obj.type = 'bp';
             obj.alpha = 0.025;
+            obj.isBias = true;
         end
         
         function neural_output = network_feedforward(obj,input)
             % Forward propagation (Input through output)
-            obj.layerInput{1} = input;
+            obj.layerInput{1} = [input;ones(1,size(input,2))];
+            %obj.layerInput{1} = [input;1];
+            %obj.layerInput{1} = [input];
             for i = 1:obj.nLayers - 1
                 obj.layerOutput{i} = obj.W{i}'*obj.layerInput{i};
                 obj.layerOutput{i} = 1./(1 + exp(-obj.layerOutput{i}));
                 if i < obj.nLayers - 1
-                    obj.layerInput{i+1} = obj.layerOutput{i};
+                    obj.layerInput{i+1} = [obj.layerOutput{i};ones(1,size(input,2))];
+                    %obj.layerInput{i+1} = [obj.layerOutput{i};1];
+                    %obj.layerInput{i+1} = [obj.layerOutput{i}];
                 end
             end            
             neural_output = obj.layerOutput{end};
@@ -35,11 +42,13 @@ classdef nnetworkBP < nnetwork
         function network_learning(obj,error)
             % Backpropagation to weights between input and hidden layer
             deltaError = obj.layerOutput{end}.*(1-obj.layerOutput{end}).*error;
-            tempW = obj.W{end};
+            tempW = obj.W{end}(1:end-1,:);
+            %tempW = obj.W{end};
             obj.W{end} = obj.W{end} + obj.alpha*obj.layerInput{end}*deltaError';
             for i = obj.nLayers-2:-1:1                          
                 deltaError = obj.layerOutput{i}.*(1-obj.layerOutput{i}).*(tempW*deltaError);
-                tempW = obj.W{i};
+                tempW = obj.W{i}(1:end-1,:);
+                %tempW = obj.W{i};
                 obj.W{i} = obj.W{i} + obj.alpha*obj.layerInput{i}*deltaError';
             end
             
